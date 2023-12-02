6f309085d17d60ef572cc43ee0996d3c2c9f5c59
XXXX11XXXX <151361564+XXXX11XXXX@users.noreply.github.com>
2023-12-02 21:31:47 +0800

    Add files via upload

---
+++ 004.js
@@ -0,0 +1,14 @@
+let arr = [1,[2,3],[4,5,[6,7,8]],9];
+function flatten(arr) {
+    let result = [];
+    for(let i=0;i<arr.lrngth;i++)
+{
+    if(Array.isArray(arr[i])){result=result.concat(flatten(arr[i]));
+
+}
+else{result.push(arr[i])}
+}
+return result;
+}
+console.log(flatten(a));
+//结果 [1,2,3,4,5,6,7,8,9]
