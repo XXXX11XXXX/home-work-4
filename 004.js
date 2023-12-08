e1c65ff361d892022b8e99efd609753b00b394cf
XXXX11XXXX <151361564+XXXX11XXXX@users.noreply.github.com>
2023-12-08 17:16:03 +0800

    Update 004.js

--- 004.js
+++ 004.js
@@ -1,7 +1,7 @@
 let arr = [1,[2,3],[4,5,[6,7,8]],9];
 function flatten(arr) {
     let result = [];
-    for(let i=0;i<arr.lrngth;i++)
+    for(let i=0;i<arr.length;i++)
 {
     if(Array.isArray(arr[i])){result=result.concat(flatten(arr[i]));
 
